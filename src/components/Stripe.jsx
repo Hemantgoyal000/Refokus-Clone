40811796ae0f4520b5c07f66b474a5c539325350
Hemant Goyal <goyalhemant431@gmail.com>
2024-10-17 18:27:46 +0530

    Update Stripe.jsx

--- src/components/Stripe.jsx
+++ src/components/Stripe.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function Stripe({val}) {
+function Stripe({ val }) {
   return (
-    <div className='px-4 py-3 border-t-[1px] border-b-[1px] border-r-[1px] border-zinc-600 flex justify-between items-center'>
-        <span className='text-lg font-medium'>{val.name}</span>
-        <span className='font-medium'>{val.number}</span>
+    <div className="px-4 py-3 border-t-[1px] border-b-[1px] border-r-[1px] border-zinc-600 flex justify-between items-center">
+      <span className="text-lg font-medium">{val.name}</span>
+      <span className="font-medium">{val.number}</span>
     </div>
-  )
+  );
 }
 
-export default Stripe
+export default Stripe;
